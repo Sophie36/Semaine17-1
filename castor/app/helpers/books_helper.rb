10d6cac0b0ac9bd7687a0985b1320cc135c30cce
sophie36 <sophielemaire361@gmail.com>
2016-02-14 19:44:03 +0000

    Deux derniers chapitres RoR

---
+++ castor/app/helpers/books_helper.rb
@@ -0,0 +1,9 @@
+module BooksHelper
+end
+
+module BooksHelper
+    def exemple
+        "Ceci est un test"
+    end
+end
+
---
+++ castor/app/models/book.rb
@@ -0,0 +1,14 @@
+# app/models/book.rb
+class Book < ActiveRecord::Base
+    belongs_to :category
+
+    validates :title, presence: {
+        message: "Le titre doit être renseigné."
+    }
+
+    validates :title, uniqueness: {
+        message: "Ce titre est déjà pris."
+    }
+    
+    scope :french, -> { where(category_id: 1) }
+end
